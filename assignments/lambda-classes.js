2cc8907908e12fe11e8b708fc8047a2b4ae93c98
Tora-Tora <53785365+Tora-Tora@users.noreply.github.com>
2019-08-22 20:05:37 -0500

    Finish creating class for Person and Instructor. Partially finish class for Student.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,69 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(obj){
+        this.name = obj.name;
+        this.age = obj.age;
+        this.location = obj.location;
+
+    }
+    speak() {
+        console.log(`Hello myname is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(obj){
+        super(obj);
+        this.specialty = obj.specialty;
+        this.favLanguage = obj.favLanguage;
+        this.catchPhrase = obj.catchPhrase;
+    }
+    demo(subject){ //subject is a string
+        console.log(`Today we are learning about ${subject}`);
+    }
+    grade(student, subject){ //student is an object, subject is a string
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person {
+    constructor(obj){
+        super(obj);
+        this.previousBackground = obj.previousBackground;
+        this.className = obj.className;
+        this.favSubjects = obj.favSubjects; // This one will be an array
+    }
+    listSubjects(){
+        console.log(`${this.name}'s favorite subjects are: `);
+        this.favSubjects.forEach( current => {
+            console.log(current);
+        });
+    }
+}
+
+
+
+
+// Stuff for testing below here
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+
+const rincewind = new Student({
+    name: 'Rincewind',
+    location: 'Ang-Morpork',
+    age: 32,
+    previousBackground: 'Messing everything up everywhere he went.',
+    className: 'WEB23',
+    favSubjects: ['magic-I', 'magic-II', 'magic-III']
+});
+
+// fred.demo('CSS');
+// rincewind.listSubjects();
